642c046fe290e1a93d8d18b5c9a07f2ef7aab069
bugthug <avivyodfat@proton.me>
2023-05-17 07:14:50 +0300

    Make homepage default page

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import './style.css';
 import {onHomeClick, onContactPageClick, onMenuPageClick} from "./pageContent";
 
-
+onHomeClick();
 document.querySelector('#home').addEventListener('click', onHomeClick);
 document.querySelector('#restaurant-menu').addEventListener('click',onMenuPageClick);
 document.querySelector('#contact').addEventListener('click', onContactPageClick);
